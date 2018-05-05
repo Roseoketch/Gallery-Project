aaedd7a501ebc622c6559c5ca5ec97d8e1a6b662
Roseoketch <oketchrose@gmail.com>
2018-05-05 18:18:58 +0300

    fuctions created

--- mygallery/pics/views.py
+++ mygallery/pics/views.py
@@ -7,7 +7,7 @@ from .models import Photos
 # Create your views here.
 def my_gallery(request):
     photos = Photos.objects.all()
-    return render(request, 'gallery.html', {'photos':photos})
+    return render(request, 'gallery.html')
 
 def single_photo(request, photo_id):
     photo = Photos.objects.get(id=photo_id)
