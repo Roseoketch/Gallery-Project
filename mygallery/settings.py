cf346153fa119c78ea0d1ec0d539e5294217a30a
Roseoketch <oketchrose@gmail.com>
2018-05-07 12:49:02 +0300

    styling

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+# "$for$8lz614zoo92d&hz*5n)njszursbn-y0=wp9swl&i_imd&"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  # config('DEBUG', default=False, cast=bool)
